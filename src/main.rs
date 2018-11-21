7386838aa478531b5cf764f2e0bf02f5afd65e55
Benjamin Hetherington <b.w.hetherington@gmail.com>
2018-11-21 12:15:46 +0100

    Removed bytes line

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,5 @@ mod app;
 mod repl;
 
 fn main() {
-    let size = std::mem::size_of::<rlisp_core::expression::Callable>();
-    println!("size = {} bytes", size);
     app::run();
-    // let mut buf = String::new();
-    // let stdin = std::io::stdin();
-    // loop {
-    //     println!("Enter a number:");
-    //     stdin.read_line(&mut buf).expect("failed to read stdin");
-    //     let res = buf.parse::<rlisp_core::quat::Quat>();
-    //     println!("res: {:?}", res);
-    //     buf.clear();
-    // }
 }
